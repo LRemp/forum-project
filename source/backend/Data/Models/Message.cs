6133db6272086ccb0a45331628ab252858c205aa
LRemp <l.lbruzga.b@gmail.com>
2023-09-24 07:57:00 +0300

    initial project structure

---
+++ source/backend/Data/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Data.Models
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int Author { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
