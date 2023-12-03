0fc4213801257cad57ee29f8a79392edb7c6b612
LRemp <l.lbruzga.b@gmail.com>
2023-12-03 17:27:13 +0200

    add Id poperty for ChannelDTO

--- source/Backend.Core/DTOs/ChannelDTO.cs
+++ source/Backend.Core/DTOs/ChannelDTO.cs
@@ -8,6 +8,7 @@ namespace Backend.Core.DTOs
 {
     public class ChannelDTO
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
